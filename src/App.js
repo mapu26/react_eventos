fa932e91b88436fca41f675bd2480bac5f6ec051
Miguel Angel Piquer Ubeda <mapu26@gmail.com>
2022-01-13 21:45:42 +0100

    Video 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import imagen0 from './imagenes/rey_atanagildo.png';
+import imagen1 from './imagenes/rey_sisebuto.png';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const refCaja = useRef(); /*Esto se hace para referenciar una cosa. En este caso del div caja*/
+  const moneda = 23.16;
+
+  const incrementar = (e) =>{
+    e.target.innerHTML = Number(e.target.innerHTML)+1; /*Incrementa en 1 el valor de la caja de abajo. Numbre es para pasar a numero*/
+    /*e.target.style.backgroundColor = "red";*/
+    if(e.target.innerHTML>=10){
+      e.target.innerHTML = 1;
+    }
+    if(e.target.innerHTML>=8){
+      e.target.style.backgroundColor = "red";
+    }else{
+      e.target.style.backgroundColor = "white";
+    }
+  }
+
+  const convertir = ()=>{  /*funcion flecha*/
+    refCaja.current.innerHTML = Number(refCaja.current.innerHTML)*moneda;
+  }
+
+  const cambio = (e) =>{
+    if(e.target.src.includes("atanagildo")){
+      e.target.src = imagen1;
+    }else{
+      e.target.src = imagen0;
+    }
+  }
+
+  const lectura = (e) =>{
+    refCaja.current.innerHTML = e.target.value
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div ref={refCaja} className='caja' onClick={incrementar}>1</div>
+      <button onClick={convertir}>Aceptar</button>
+      <div> <img onClick={cambio} src={imagen0}/></div>
+      <input onChange={lectura} className='campo'/>
+    </>
   );
 }
 
